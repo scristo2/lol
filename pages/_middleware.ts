7ad1eb6425818922c5b2aa0105768dbc6af77062
scristo2 <sergio_colino_c@hotmail.com>
2022-01-23 12:20:54 +0100

    update 23/01/22

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,7 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req : NextRequest, res : NextResponse){
    
      const response = NextResponse.next();
-     response.cookie("ip", req.ip || "undefined");
+     response.cookie("ip", req.ip || "undefined", {
+
+          path : "/",
+          maxAge : 3600,
+          httpOnly : true,
+          sameSite : "strict",
+          domain : "leagueoflegendspremium.com"
+     });
      return response;
         
 }
